0eeafa913c4162840513a490199262abd6f9898c
Alan Jeferson <alan.jeferson11@gmail.com>
2017-02-13 03:47:38 +0000

    Fix the tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 
   it 'checks the name' do
 	@user = User.new
-	expect(@user.save).to eq(true)
+	expect(@user.save).to eq(false)
 	@user.name = 'Kakaroto'
 	expect(@user.save).to eq(true)
   end
